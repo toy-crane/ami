6ccb80f95db480cb7cc6a9876c26da2e4c23848e
toycrane <alwaysfun2183@gmail.com>
2020-10-14 18:22:47 +0900

    apollo client setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store/index";
+import client from "./apollo/client";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<ApolloProvider client={client}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</ApolloProvider>,
 	document.getElementById("root")
 );
