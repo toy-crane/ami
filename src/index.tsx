0ea44a7ca61182ea89e92399a51cd6a6f2eca2fd
toycrane <alwaysfun2183@gmail.com>
2020-10-15 16:37:37 +0900

    react-cookie setting

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store/index";
 import client from "./apollo/client";
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</ApolloProvider>,
+	<CookiesProvider>
+		<ApolloProvider client={client}>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</ApolloProvider>
+	</CookiesProvider>,
 	document.getElementById("root")
 );
