98f5059ca804587aa2134a152d361f063dcf42d6
toycrane <alwaysfun2183@gmail.com>
2020-10-15 16:39:08 +0900

    redux 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
 import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import App from "./App";
-import store from "./store/index";
 import client from "./apollo/client";
 import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
 	<CookiesProvider>
 		<ApolloProvider client={client}>
-			<Provider store={store}>
-				<App />
-			</Provider>
+			<App />
 		</ApolloProvider>
 	</CookiesProvider>,
 	document.getElementById("root")
--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./root-reducer";
-
-const initialState = {};
-
-// create store
-const store = createStore(rootReducer, initialState);
-export default store;
