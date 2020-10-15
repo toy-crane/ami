7a86493518570703eb3515020121cec4b76de7fc
toycrane <alwaysfun2183@gmail.com>
2020-10-15 16:11:49 +0900

    rename token service

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,5 +1,6 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import tokenService from "../services/tokenService";
 
 const httpLink = createHttpLink({
 	uri: "http://localhost:4000/graphql",
@@ -7,7 +8,7 @@ const httpLink = createHttpLink({
 
 // token이 있으면 요청마다 항상 token을 실어서 보낸다.
 const authLink = setContext((_, { headers }) => {
-	const token = localStorage.getItem("token");
+	const token = tokenService.get();
 	return {
 		headers: {
 			...headers,
