64df1d5ecbfc50e11074133aa71a7a4ec3a55480
toycrane <alwaysfun2183@gmail.com>
2020-10-15 13:06:57 +0900

    token 실어서 request 보내기

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+	uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem("token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000",
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
 
