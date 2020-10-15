708dcbe29d98eff676145f833e79198f7b44141c
toycrane <alwaysfun2183@gmail.com>
2020-10-15 15:07:29 +0900

    주석 추가

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -5,6 +5,7 @@ const httpLink = createHttpLink({
 	uri: "http://localhost:4000/graphql",
 });
 
+// token이 있으면 요청마다 항상 token을 실어서 보낸다.
 const authLink = setContext((_, { headers }) => {
 	const token = localStorage.getItem("token");
 	return {
