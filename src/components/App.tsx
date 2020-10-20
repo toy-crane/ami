434b6d431b4301c294246d530b66b40869a62a97
toycrane <alwaysfun2183@gmail.com>
2020-10-20 17:46:27 +0900

    App.tsx 위치 변경

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
-import AuthPage from "./components/AuthPage";
-import MainPage from "./components/MainPage";
-import { useApolloClient } from "./apollo/client";
-import { withAuth } from "./services/auth";
+import AuthPage from "./AuthPage";
+import MainPage from "./MainPage";
+import { useApolloClient } from "../apollo/client";
+import { withAuth } from "../services/auth";
 function App() {
 	const apolloClient = useApolloClient();
 	return (
