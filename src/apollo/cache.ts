4d989fa09805dc369f1de0c3bd64bbd7f59b7be0
toycrane <alwaysfun2183@gmail.com>
2020-10-29 14:08:16 +0900

    안쓰는 cache 삭제

--- src/apollo/cache.ts
+++
@@ -1,9 +0,0 @@
-import { makeVar } from "@apollo/client";
-
-type temporaryUserType = {
-	email: string | null;
-};
-
-export const temporaryUserVar = makeVar<temporaryUserType>({
-	email: null,
-});
