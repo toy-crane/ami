6979a821544fcf03dccd640fb6df5a3ff7892b95
toycrane <alwaysfun2183@gmail.com>
2020-11-09 19:14:48 +0900

    깃헙 로그인 추가

--- src/components/Auth/SignUpForm.tsx
+++ src/components/Auth/SignUpForm.tsx
@@ -48,39 +48,43 @@ const SignUpForm = () => {
 			{loading ? (
 				<div>Loading... </div>
 			) : (
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<div>
-						<label>이메일</label>
-						<input type="text" name="email" ref={register} />
-						<p>{ValidationErrors.email?.message}</p>
-					</div>
-					<div>
-						<label>닉네임</label>
-						<input type="text" name="username" ref={register} />
-						<p>{ValidationErrors.username?.message}</p>
-					</div>
-					<div>
-						<label>이름</label>
-						<input name="name" ref={register} />
-						<p>{ValidationErrors.name?.message}</p>
-					</div>
-					<div>
-						<label>비밀번호</label>
-						<input name="password" type="password" ref={register} />
-						<p>{ValidationErrors.password?.message}</p>
-					</div>
-					<div>
-						<label>비밀번호 확인</label>
-						<input name="confirmPassword" type="password" ref={register} />
-						<p>{ValidationErrors.confirmPassword?.message}</p>
-					</div>
-					<input
-						type="submit"
-						disabled={!formState.isValid}
-						value="회원 가입"
-					/>
-					<ErrorMessage data-testid="error-message">{error}</ErrorMessage>
-				</form>
+				<div>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<label>이메일</label>
+							<input type="text" name="email" ref={register} />
+							<p>{ValidationErrors.email?.message}</p>
+						</div>
+						<div>
+							<label>닉네임</label>
+							<input type="text" name="username" ref={register} />
+							<p>{ValidationErrors.username?.message}</p>
+						</div>
+						<div>
+							<label>이름</label>
+							<input name="name" ref={register} />
+							<p>{ValidationErrors.name?.message}</p>
+						</div>
+						<div>
+							<label>비밀번호</label>
+							<input name="password" type="password" ref={register} />
+							<p>{ValidationErrors.password?.message}</p>
+						</div>
+						<div>
+							<label>비밀번호 확인</label>
+							<input name="confirmPassword" type="password" ref={register} />
+							<p>{ValidationErrors.confirmPassword?.message}</p>
+						</div>
+						<input
+							type="submit"
+							disabled={!formState.isValid}
+							value="회원 가입"
+						/>
+						<ErrorMessage data-testid="error-message">{error}</ErrorMessage>
+					</form>
+					<a href="http://localhost:4000/github">github</a>
+					<a href="http://localhost:4000/google">google</a>
+				</div>
 			)}
 		</div>
 	);
