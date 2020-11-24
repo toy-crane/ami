7c12fbc7ea8b8872be60c94ba3d90b17ac765f7e
toycrane <alwaysfun2183@gmail.com>
2020-11-24 18:11:49 +0900

    auth Link 수정

--- src/apollo/authLink.ts
+++ src/apollo/authLink.ts
@@ -1,14 +1,21 @@
-import { setContext } from "@apollo/client/link/context";
+import { ApolloLink } from "@apollo/client";
 import { isLoggedInVar } from "./cache";
 
-const authLink = setContext((_, { headers }) => {
-	const token = isLoggedInVar();
-	return {
+type Headers = {
+	authorization?: string;
+};
+
+// 매 요청마다 토큰을 실어보내는 Link
+const authLink = new ApolloLink((operation, forward) => {
+	const accessToken = isLoggedInVar();
+	operation.setContext(({ headers }: { headers: Headers }) => ({
 		headers: {
 			...headers,
-			authorization: token ? `Bearer ${token}` : "",
+			authorization: accessToken ? `Bearer ${accessToken}` : "",
 		},
-	};
+	}));
+
+	return forward(operation);
 });
 
 export default authLink;
