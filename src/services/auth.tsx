10cedcf21bdbdd06675ffb62d47e50b95a91c3bb
toycrane <alwaysfun2183@gmail.com>
2020-11-25 18:03:43 +0900

    메모리 토큰 초기화 코드 추가

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -1,6 +1,7 @@
 import { useGetMeQuery } from "../generated/graphql";
 import { useApolloClient } from "@apollo/client";
 import { useHistory } from "react-router-dom";
+import { accessTokenVar } from "../apollo/cache";
 
 // useGetMe hooks
 export const useGetMe = useGetMeQuery;
@@ -9,7 +10,10 @@ export const useLogout = () => {
 	const apolloClient = useApolloClient();
 	const history = useHistory();
 	const logout = async () => {
+		// cache 초기화
 		await apolloClient.clearStore();
+		// 메모리에 있는 토큰 초기화
+		accessTokenVar(null);
 		history.push("/");
 	};
 	return logout;
