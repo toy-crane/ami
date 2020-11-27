8c3b6bbe85371b66955f315801f2a822db7929f6
toycrane <alwaysfun2183@gmail.com>
2020-11-27 18:41:47 +0900

    NotFoundPage 추가

---
+++ src/Routes/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const NotFoundPage = () => {
+	console.log("hello");
+	return (
+		<>
+			<div>Page Not Found 404</div>
+		</>
+	);
+};
+
+export default NotFoundPage;
