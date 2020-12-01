e59d2ed026b108f8218714da58e250577783db14
toycrane <alwaysfun2183@gmail.com>
2020-12-02 00:33:19 +0900

    시스템 폰트 사용하도록 변경

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,15 @@ import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
 import { Router } from "react-router-dom";
 import { history } from "./services/browserHistory";
-import { ThemeProvider } from "@emotion/react";
+import { ThemeProvider, Global } from "@emotion/react";
 import Theme from "./Styles/Theme";
 import "./index.css";
+import GlobalStyles from "./Styles/GlobalStyles";
 
 ReactDOM.render(
 	<ApolloProvider client={client}>
 		<ThemeProvider theme={Theme}>
+			<Global styles={GlobalStyles} />
 			<Router history={history}>
 				<App />
 			</Router>
