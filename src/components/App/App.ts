17770d44bcc4cb10132c278196b57f7cadd191a6
toycrane <alwaysfun2183@gmail.com>
2020-12-01 12:02:55 +0900

    tsx -> ts

---
+++ src/components/App/App.ts
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Routes from "../Routes";
+import Header from "../Header";
+
+const Wrapper = styled.div`
+	margin: 0 auto;
+	max-width: 1600px;
+	width: 100%;
+`;
+
+function App() {
+	return (
+		<>
+			<Wrapper>
+				<Header></Header>
+				<Routes></Routes>
+			</Wrapper>
+		</>
+	);
+}
+
+export default App;
