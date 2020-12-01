ea6c3f66bdd0ebbd8c2332a93efa46d62f726b99
toycrane <alwaysfun2183@gmail.com>
2020-12-01 16:14:46 +0900

    activateUser UI 구현

--- src/components/Auth/ActivateUser/ActivateUser.tsx
+++ src/components/Auth/ActivateUser/ActivateUser.tsx
@@ -1,10 +1,39 @@
 import React from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { nameValidator, mobileValidator } from "../validation/schema";
+
+export const schema = yup.object().shape({
+	name: nameValidator,
+	mobile: mobileValidator,
+});
+
+interface IFormInputs {
+	mobile: string;
+	name: string;
+}
 
 const ActivateUser = () => {
+	const { register, handleSubmit, errors } = useForm<IFormInputs>({
+		resolver: yupResolver(schema),
+	});
+	const onSubmit = (data: IFormInputs) => console.log(data);
+
 	return (
-		<>
-			<div>유저 활성화 페이지</div>
-		</>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div>
+				<label htmlFor="name">name</label>
+				<input type="text" name="name" ref={register} />
+				<p>{errors.name?.message}</p>
+			</div>
+			<div>
+				<label htmlFor="mobile">mobile</label>
+				<input type="text" name="mobile" ref={register} />
+				<p>{errors.mobile?.message}</p>
+			</div>
+			<input type="submit" />
+		</form>
 	);
 };
 
--- src/components/Auth/validation/schema.ts
+++ src/components/Auth/validation/schema.ts
@@ -1,50 +1,18 @@
 import * as yup from "yup";
 
-const userNameValidator = yup
-	.string()
-	.min(6, "닉네임은 최소 6글자 이상이여야 합니다.")
-	.max(20, "닉네임은 최대 20글자입니다.")
-	.required();
-
 const nameValidator = yup
 	.string()
 	.min(2, "이름은 최소 2글자 이상입니다.")
 	.max(20, "이름은 최대 20글자까지 입니다.")
 	.required();
 
-const passwordValidator = yup
+const mobileValidator = yup
 	.string()
-	.min(10, "비밀번호는 최소 10글자 이상입니다.")
-	.max(256, "비밀번호는 최대 256글자까지 입니다.")
-	.required();
+	.min(10, "휴대폰 번호가 잘못 되었습니다.")
+	.required()
+	.matches(
+		/^(?:(010-\d{4})|(01[1|6|7|8|9]-\d{3,4}))(-\d{4})$/,
+		"휴대폰 번호가 잘못 되었습니다."
+	);
 
-const emailValidator = yup
-	.string()
-	.email("유효한 이메일 주소가 아닙니다.")
-	.required();
-
-export const signUpSchema = yup.object().shape({
-	email: emailValidator,
-	username: userNameValidator,
-	name: nameValidator,
-	password: passwordValidator,
-	confirmPassword: yup
-		.string()
-		.oneOf([yup.ref("password"), undefined], "비밀번호가 일치하지 않습니다."),
-});
-
-export const signInSchema = yup.object().shape({
-	email: emailValidator,
-	password: passwordValidator,
-});
-
-export const requestPasswordResetSchema = yup.object().shape({
-	email: emailValidator,
-});
-
-export const resetPasswordSchema = yup.object().shape({
-	password: passwordValidator,
-	confirmPassword: yup
-		.string()
-		.oneOf([yup.ref("password"), undefined], "비밀번호가 일치하지 않습니다."),
-});
+export { nameValidator, mobileValidator };
