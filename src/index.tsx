d57122e107ecd00eed23803f7a7c578f27969a87
toycrane <alwaysfun2183@gmail.com>
2020-12-01 23:45:21 +0900

    안쓰는 provider 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
-import { CookiesProvider } from "react-cookie";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
-import { Router, Route } from "react-router-dom";
-import { QueryParamProvider } from "use-query-params";
+import { Router } from "react-router-dom";
 import { history } from "./services/browserHistory";
 import "./index.css";
 
 ReactDOM.render(
 	<ApolloProvider client={client}>
-		<CookiesProvider>
-			<Router history={history}>
-				<QueryParamProvider ReactRouterRoute={Route}>
-					<App />
-				</QueryParamProvider>
-			</Router>
-		</CookiesProvider>
+		<Router history={history}>
+			<App />
+		</Router>
 	</ApolloProvider>,
 	document.getElementById("root")
 );
