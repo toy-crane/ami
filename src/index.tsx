840722a99ecaccaad54efae63c142b79a0b6ae1e
toycrane <alwaysfun2183@gmail.com>
2020-12-01 12:02:17 +0900

    App.tsx 위치 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { App } from "./components/App";
 import { CookiesProvider } from "react-cookie";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
