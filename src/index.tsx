c6f9af6ea55bdc057753f4842d83d3f3a7643496
toycrane <alwaysfun2183@gmail.com>
2020-12-02 00:15:15 +0900

    add theme

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,17 @@ import { ApolloProvider } from "@apollo/client";
 import client from "./apollo/client";
 import { Router } from "react-router-dom";
 import { history } from "./services/browserHistory";
+import { ThemeProvider } from "@emotion/react";
+import Theme from "./Styles/Theme";
 import "./index.css";
 
 ReactDOM.render(
 	<ApolloProvider client={client}>
-		<Router history={history}>
-			<App />
-		</Router>
+		<ThemeProvider theme={Theme}>
+			<Router history={history}>
+				<App />
+			</Router>
+		</ThemeProvider>
 	</ApolloProvider>,
 	document.getElementById("root")
 );
