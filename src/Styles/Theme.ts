8f82a869a1c7707fa3ac03030d461de46cd95aab
toycrane <alwaysfun2183@gmail.com>
2020-12-02 15:59:57 +0900

    default theme 수정

--- src/Styles/Theme.ts
+++ src/Styles/Theme.ts
@@ -1,18 +1,32 @@
 export default {
 	fontSizes: [12, 14, 16, 24, 32, 48, 64],
+	breakpoints: ["40em", "52em", "64em"],
+	space: [0, 4, 8, 16, 32, 64, 128, 256],
 	colors: {
 		primary: "#07c",
 		gray: "#f6f6ff",
+	},
+	font: {
+		body:
+			"-apple-system, BlinkMacSystemFont, Bazier Square, Noto Sans KR, Segoe UI, Apple SD Gothic Neo, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+		heading:
+			"-apple-system, BlinkMacSystemFont, Bazier Square, Noto Sans KR, Segoe UI, Apple SD Gothic Neo, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+		monospace:
+			"-apple-system, BlinkMacSystemFont, Bazier Square, Noto Sans KR, Segoe UI, Apple SD Gothic Neo, Roboto, Helvetica Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+	},
+	fontWeights: {
+		body: 400,
+		heading: 700,
+		bold: 700,
+	},
+	lineHeights: {
+		body: 1.5,
+		heading: 1.25,
 	},
 	buttons: {
 		primary: {
 			color: "white",
 			bg: "primary",
-		},
-		outline: {
-			color: "primary",
-			bg: "transparent",
-			boxShadow: "inset 0 0 0 2px",
 		},
 	},
 };
