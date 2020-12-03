071256d707c859af2e68d4ea41a8ae00fce2122b
toycrane <alwaysfun2183@gmail.com>
2020-12-03 15:26:59 +0900

    alt 속성 추가

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = () => (
-	<img src="/logo/black-logo.svg" width="128px" height="32px" />
+	<img src="/logo/black-logo.svg" width="128px" height="32px" alt="logo" />
 );
 
 export default Logo;
