4d20a4084b0a1c68578be5cdc12f9a0b8aa681c9
toycrane <alwaysfun2183@gmail.com>
2020-12-03 15:26:14 +0900

    Logo Component 변경

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Image } from "rebass";
 
 const Logo = () => (
-	<Image src="/logo/test-logo.svg" width="128px" height="32px" />
+	<img src="/logo/black-logo.svg" width="128px" height="32px" />
 );
 
 export default Logo;
