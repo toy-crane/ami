7449b4bfee01662794aff6c05e7a0909d60f7653
toycrane <alwaysfun2183@gmail.com>
2020-12-05 01:49:07 +0900

    logo 이미지 변경

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,11 +2,7 @@
 import { jsx, Image } from "theme-ui";
 
 const Logo = () => (
-		<Image
-			src="/logo/black-logo.svg"
-			sx={{ variant: "images.mainLogo" }}
-			alt="logo"
-		/>
+	<Image src="/logo/logo.svg" sx={{ variant: "images.mainLogo" }} alt="logo" />
 );
 
 export default Logo;
