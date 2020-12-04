7deed9b19a88396bf2a98877757d5d182f335216
toycrane <alwaysfun2183@gmail.com>
2020-12-05 02:09:31 +0900

    미니멀한 로고로 변경

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,11 @@
 import { jsx, Image } from "theme-ui";
 
 const Logo = () => (
-	<Image src="/logo/logo.svg" sx={{ variant: "images.mainLogo" }} alt="logo" />
+	<Image
+		src="/logo/black-logo.svg"
+		sx={{ variant: "images.mainLogo" }}
+		alt="logo"
+	/>
 );
 
 export default Logo;
