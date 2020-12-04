02ba08b9ee5186f1dfee9a46745441e93f5c559d
toycrane <alwaysfun2183@gmail.com>
2020-12-04 15:37:20 +0900

    theme type bug 수정

--- src/Styles/Theme.ts
+++ src/Styles/Theme.ts
@@ -1,21 +1,17 @@
-import theme from "@theme-ui/preset-base";
+import { Theme } from "@theme-ui/css";
 
-export default {
-	...theme,
-	fontSizes: [12, 14, 16, 24, 32, 48, 64],
-	breakpoints: ["40em", "52em", "64em"],
-	space: [0, 4, 8, 16, 32, 64, 128, 256],
+const theme: Theme = {
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
 	sizes: {
 		container: 1024,
 	},
 	fonts: {
 		body:
-			'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-		heading:
-			'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-		monospace:
-			'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+			'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+		heading: "inherit",
+		monospace: "Menlo, monospace",
 	},
+	fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
 	fontWeights: {
 		body: 400,
 		heading: 700,
@@ -23,6 +19,99 @@ export default {
 	},
 	lineHeights: {
 		body: 1.5,
-		heading: 1.25,
+		heading: 1.125,
+	},
+	colors: {
+		text: "#000",
+		background: "#fff",
+		primary: "#07c",
+		secondary: "#30c",
+		muted: "#f6f6f6",
+	},
+	styles: {
+		root: {
+			fontFamily: "body",
+			lineHeight: "body",
+			fontWeight: "body",
+		},
+		h1: {
+			color: "text",
+			fontFamily: "heading",
+			lineHeight: "heading",
+			fontWeight: "heading",
+			fontSize: 5,
+		},
+		h2: {
+			color: "text",
+			fontFamily: "heading",
+			lineHeight: "heading",
+			fontWeight: "heading",
+			fontSize: 4,
+		},
+		h3: {
+			color: "text",
+			fontFamily: "heading",
+			lineHeight: "heading",
+			fontWeight: "heading",
+			fontSize: 3,
+		},
+		h4: {
+			color: "text",
+			fontFamily: "heading",
+			lineHeight: "heading",
+			fontWeight: "heading",
+			fontSize: 2,
+		},
+		h5: {
+			color: "text",
+			fontFamily: "heading",
+			lineHeight: "heading",
+			fontWeight: "heading",
+			fontSize: 1,
+		},
+		h6: {
+			color: "text",
+			fontFamily: "heading",
+			lineHeight: "heading",
+			fontWeight: "heading",
+			fontSize: 0,
+		},
+		p: {
+			color: "text",
+			fontFamily: "body",
+			fontWeight: "body",
+			lineHeight: "body",
+		},
+		a: {
+			color: "primary",
+		},
+		pre: {
+			fontFamily: "monospace",
+			overflowX: "auto",
+			code: {
+				color: "inherit",
+			},
+		},
+		code: {
+			fontFamily: "monospace",
+			fontSize: "inherit",
+		},
+		table: {
+			width: "100%",
+			borderCollapse: "separate",
+			borderSpacing: 0,
+		},
+		th: {
+			textAlign: "left",
+			borderBottomStyle: "solid",
+		},
+		td: {
+			textAlign: "left",
+			borderBottomStyle: "solid",
+		},
+		img: {
+			maxWidth: "100%",
+		},
 	},
 };
+export default theme;
