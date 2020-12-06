8c9db448d388d4fa0d24ea3ae6cc985e7199b906
toycrane <alwaysfun2183@gmail.com>
2020-12-06 12:02:04 +0900

    grid 셋팅

--- src/components/Main/MainPage.tsx
+++ src/components/Main/MainPage.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { AspectImage, Flex, Heading, jsx, Image, Box, Grid } from "theme-ui";
 
 const Main = () => {
-	return <div>메인 페이지</div>;
+	return (
+		<Grid gap={1} columns={"repeat(auto-fit, minmax(300px, 1fr))"}>
+			<Box sx={{ "align-self": "center" }}>
+				<Heading>나도 프로덕트 잘 만들고 싶다.</Heading>
+			</Box>
+			<Box>
+				<Image src="/illustration/undraw_pair_programming.svg"></Image>;
+			</Box>
+		</Grid>
+	);
 };
 
 export default Main;
+
+{
+	/* <Flex
+			sx={{
+				alignItems: "center",
+			}}
+		>
+			<Box>
+				<Heading>나도 프로덕트 잘 만들고 싶다.</Heading>
+			</Box>
+			<Box>
+				<Image src="/illustration/code_review.svg"></Image>;
+			</Box>
+		</Flex> */
+}
