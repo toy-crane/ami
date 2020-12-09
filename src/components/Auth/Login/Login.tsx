44570f40dd4ee796b066f47d3009e4feb182cda9
toycrane <alwaysfun2183@gmail.com>
2020-12-09 16:17:31 +0900

    로그인 페이지 구현

--- src/components/Auth/Login/Login.tsx
+++ src/components/Auth/Login/Login.tsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Link, Flex, Heading, Image, Text } from "theme-ui";
+import GitHubButton from "../../Button/GithubButton";
 
 const Login = () => {
 	return (
-		<>
-			<div>
-				로그인 페이지
-				<div>
-					<a href="http://localhost:4000/github">github 로그인</a>
-				</div>
-				<Link to="/sign-up">아직 회원이 아니신가요?</Link>
-			</div>
-		</>
+		<Flex
+			sx={{
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			<Heading as="h1" my={3}>
+				로그인
+			</Heading>
+			<Image src="/illustration/login.svg" my={4} />
+			<GitHubButton
+				text="github으로 로그인하기"
+				url="http://localhost:4000/github"
+			/>
+			<Text my={2}>
+				<Link href="/sign-up" mx={1}>
+					코드팟 계정이 없으신가요?
+				</Link>
+			</Text>
+		</Flex>
 	);
 };
 
