6969847d852ac8ee886ef20be619253b1f7ec231
toycrane <alwaysfun2183@gmail.com>
2020-12-09 16:34:15 +0900

    NotFound Page 구현

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Image, Button, Flex, Heading, Text } from "theme-ui";
+import Container from "../Layout/Container";
+import { useHistory } from "react-router-dom";
 
 const NotFound = () => {
-	console.log("hello");
+	let history = useHistory();
+
 	return (
-		<>
-			<div>Page Not Found 404</div>
-		</>
+		<Container sx={{ width: "480px" }}>
+			<Flex
+				sx={{
+					justifyContent: "center",
+					alignItems: "center",
+					flexDirection: "column",
+				}}
+			>
+				<Heading as="h1" my={3}>
+					Page Not Found....
+					<span role="img" aria-label="Sad">
+						😭😭
+					</span>
+				</Heading>
+				<Image src="/illustration/404_notfound.svg" my={4} />
+				<Button
+					my={2}
+					onClick={() => {
+						history.goBack();
+					}}
+				>
+					돌아가기
+				</Button>
+			</Flex>
+		</Container>
 	);
 };
 
