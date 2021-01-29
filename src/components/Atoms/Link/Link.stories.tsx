5da2f5edb966d5de10f11451f328578296e9bd9e
toycrane <alwaysfun2183@gmail.com>
2021-01-29 17:12:56 +0900

    create IconLink

--- src/components/Atoms/Link/Link.stories.tsx
+++ src/components/Atoms/Link/Link.stories.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Story } from "@storybook/react";
 import Link, { LinkProps } from "./index";
+import Icon from "../Icon";
 
 export default {
 	title: "Components/Link",
 	component: Link,
 };
 
-const Template: Story<LinkProps> = (args) => (
+const BasicLinkTemplate: Story<LinkProps> = (args) => (
 	<Link {...args} href="#">
 		Link Text
 	</Link>
 );
-export const Basic = Template.bind({});
+export const Basic = BasicLinkTemplate.bind({});
+
+const IconLinkTemplate: Story<LinkProps> = (args) => (
+	<Link {...args} href="#">
+		<Icon icon="menu"></Icon>
+	</Link>
+);
+
+export const IconLink = IconLinkTemplate.bind({});
