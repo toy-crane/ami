36919bb368ab900e9430a958939b87e9836a170c
toycrane <alwaysfun2183@gmail.com>
2021-02-04 17:00:14 +0900

    Icon Link 삭제

--- src/components/Atoms/Icon/index.tsx
+++ src/components/Atoms/Icon/index.tsx
@@ -1,41 +1,29 @@
-import React from "react";
-import { Flex, Link } from "theme-ui";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Flex } from "theme-ui";
 import { IconName, ICONS } from "./constants";
 interface IconProps {
 	icon: IconName;
 	width?: number;
 	height?: number;
-	hasLink?: boolean;
-	href?: string;
 }
 
 const Icon: React.FC<IconProps> = (props: IconProps) => {
-	const IconComponent = (
+	return (
 		<Flex sx={{ alignItems: "center" }} {...props}>
 			<svg
 				width={props.width}
 				height={props.height ? props.height : 24}
 				viewBox="0 0 20 14"
-				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
+				sx={{
+					display: "block",
+					margin: 0,
+				}}
 			>
-				<path
-					fill-rule="evenodd"
-					clip-rule="evenodd"
-					d={ICONS[props.icon]}
-					fill="#1A1A1A"
-				/>
+				<path d={ICONS[props.icon]} fill="#1A1A1A" />
 			</svg>
 		</Flex>
-	);
-	return (
-		<>
-			{props.hasLink && props.href ? (
-				<Link href={props.href}>{IconComponent}</Link>
-			) : (
-				IconComponent
-			)}
-		</>
 	);
 };
 
