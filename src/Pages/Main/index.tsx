f5b0e986636715c738d5ada4500839fb78aa573b
toycrane <alwaysfun2183@gmail.com>
2021-02-26 15:53:55 +0900

    Main Page에 CardGroup 추가

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -2,12 +2,14 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import DescSection from "../../components/Molecules/DescSection";
+import CardGroup from "../../components/Organisms/CardGroup";
 import Hero from "../../components/Organisms/Hero";
 import MainTemplate from "./templates";
 const Main = () => {
 	return (
 		<MainTemplate
 			hero={<Hero />}
+			classCardGroup={<CardGroup></CardGroup>}
 			DescSectionGroup={[
 				<DescSection type={0} />,
 				<DescSection type={1} />,
--- src/Pages/Main/templates/index.tsx
+++ src/Pages/Main/templates/index.tsx
@@ -6,6 +6,7 @@ import BaseTemplate, { Container } from "../../BaseTemplates/templates";
 
 interface MainTemplateProps {
 	hero: React.ReactNode;
+	classCardGroup: React.ReactNode;
 	DescSectionGroup: React.ReactNode[];
 }
 
@@ -14,6 +15,10 @@ interface DescSectionContainerProps {
 }
 
 const HeroContainer: React.FC = (props) => (
+	<Container py={[5, 6]}>{props.children}</Container>
+);
+
+const ClassCardGroupContainer: React.FC = (props) => (
 	<Container py={[5, 6]} baseColor="gray0">
 		{props.children}
 	</Container>
@@ -28,10 +33,12 @@ const DescSectionContainer: React.FC<DescSectionContainerProps> = (props) => (
 const MainTemplate: React.FC<MainTemplateProps> = ({
 	hero,
 	DescSectionGroup,
+	classCardGroup,
 }) => {
 	return (
 		<BaseTemplate>
 			<HeroContainer>{hero}</HeroContainer>
+			<ClassCardGroupContainer>{classCardGroup}</ClassCardGroupContainer>
 			{DescSectionGroup.map((DescSection, index) =>
 				index % 2 === 0 ? (
 					<DescSectionContainer>{DescSection}</DescSectionContainer>
