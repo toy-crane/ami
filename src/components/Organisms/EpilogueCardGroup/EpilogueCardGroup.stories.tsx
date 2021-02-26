835b4dd67b9a5029a24e2bc02c37143ac260cc6f
toycrane <alwaysfun2183@gmail.com>
2021-02-27 01:50:13 +0900

    안쓰는 코드 삭제

--- src/components/Organisms/EpilogueCardGroup/EpilogueCardGroup.stories.tsx
+++ src/components/Organisms/EpilogueCardGroup/EpilogueCardGroup.stories.tsx
@@ -1,50 +1,10 @@
 import React from "react";
 import { Story } from "@storybook/react";
-import ClassCardGroup from ".";
-import { ClassCardProps } from "../../Molecules/ClassCard";
 import EpilogueCardGroup from ".";
 export default {
 	title: "Organisms/Epilogue Card Group",
 	component: EpilogueCardGroup,
 };
-
-export const sampleCards: ClassCardProps[] = [
-	{
-		id: "1",
-		language: "typescript",
-		status: "inProgress",
-		to: "/",
-		start_at: new Date(),
-	},
-	{
-		id: "2",
-		language: "graphql",
-		status: "close",
-		to: "/",
-		start_at: new Date(),
-	},
-	{
-		id: "3",
-		language: "javascript",
-		status: "close",
-		to: "/",
-		start_at: new Date(),
-	},
-	{
-		id: "4",
-		language: "react",
-		status: "close",
-		to: "/",
-		start_at: new Date(),
-	},
-	{
-		id: "5",
-		language: "python",
-		status: "close",
-		to: "/",
-		start_at: new Date(),
-	},
-];
 
 const Template: Story = () => <EpilogueCardGroup></EpilogueCardGroup>;
 export const Basic = Template.bind({});
