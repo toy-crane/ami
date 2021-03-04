b398c48e3079ebded3a2030a881e21a2b6a95164
toycrane <alwaysfun2183@gmail.com>
2021-03-04 21:50:35 +0900

    Icon viewBox 수정

--- src/components/Atoms/Icon/index.tsx
+++ src/components/Atoms/Icon/index.tsx
@@ -1,6 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, Flex } from "theme-ui";
+import { jsx } from "theme-ui";
 import { ICONS } from "./constants";
 export interface IconProps {
 	icon: ICONS;
@@ -10,23 +10,18 @@ export interface IconProps {
 
 const Icon: React.FC<IconProps> = (props: IconProps) => {
 	return (
-		<Flex sx={{ alignItems: "center" }} {...props}>
-			<svg
-				width={props.size ? props.size : 24}
-				height={props.size ? props.size : 24}
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-				sx={{
-					display: "block",
-					margin: 0,
-				}}
-			>
-				<path
-					d={props.icon}
-					fill={props.color ? props.color : "currentColor"}
-				/>
-			</svg>
-		</Flex>
+		<svg
+			width={props.size ? props.size : 24}
+			height={props.size ? props.size : 24}
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			sx={{
+				display: "inline",
+				margin: 0,
+			}}
+		>
+			<path d={props.icon} fill="currentColor" />
+		</svg>
 	);
 };
 
