d59e66c62766b8abc495f1a2b5696b9d5b9f2cd2
toycrane <alwaysfun2183@gmail.com>
2021-03-04 13:59:46 +0900

    Icon vector 범용화 코드 수정

--- src/components/Atoms/Icon/Icon.stories.tsx
+++ src/components/Atoms/Icon/Icon.stories.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Story } from "@storybook/react";
-import Icon from "./index";
+import Icon, { IconProps } from "./index";
 import { ICONS } from "./constants";
 export default {
 	title: "Atoms/Icon",
 	component: Icon,
+	icon: {
+		variant: {
+			control: {
+				type: "select",
+				options: ICONS,
+			},
+		},
+	},
 };
 
-const Template: Story = (args) => <Icon {...args} icon={ICONS.menu}></Icon>;
+const Template: Story<IconProps> = (args) => <Icon {...args}></Icon>;
 export const Basic = Template.bind({});
