7c206f07be645a728279c12b76038876cf364183
toycrane <alwaysfun2183@gmail.com>
2021-03-05 13:33:13 +0900

    Global Style 추가

---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+const globalStyle = css`
+	* {
+		box-sizing: border-box;
+	}
+`;
+
+export default globalStyle;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
+import { Global } from "@emotion/react";
 import React from "react";
+import globalStyle from "../../Styles/GlobalStyle";
 import { AppRouter } from "../Router";
 
-
-
-
 function App() {
-	return <AppRouter></AppRouter>;
+	return (
+		<>
+			<Global styles={globalStyle} />
+			<AppRouter></AppRouter>
+		</>
+	);
 }
 
 export default App;
