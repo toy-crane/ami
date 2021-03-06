835531e994735f24343544ead7908d8760c89270
toycrane <alwaysfun2183@gmail.com>
2021-03-06 23:31:07 +0900

    storybook 설정 변경

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,5 +2,9 @@
 import { addDecorator } from "@storybook/react";
 import { jsx, ThemeProvider } from "theme-ui";
 import theme from "../src/Styles/Theme";
+import StoryRouter from "storybook-react-router";
 
+// theme provider 사용하기
 addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+// react router 사용
+addDecorator(StoryRouter());
