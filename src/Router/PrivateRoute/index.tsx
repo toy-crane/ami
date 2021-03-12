70c50426411d4750656d22c830d9519df53ee390
toycrane <alwaysfun2183@gmail.com>
2021-03-12 16:16:53 +0900

    Private Route에 로그인 여부 추가

--- src/Router/PrivateRoute/index.tsx
+++ src/Router/PrivateRoute/index.tsx
@@ -1,9 +1,11 @@
+import { useReactiveVar } from "@apollo/client";
+import { accountInfoCache } from "caches";
 import React from "react";
 import { Route, Redirect, RouteProps } from "react-router-dom";
 
 // 로그인 필요 페이지 전용 Route
 const PrivateRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
-	const isLoggedIn = false;
+	const { isLoggedIn } = useReactiveVar(accountInfoCache);
 
 	const handleRoute = (props: any) => {
 		if (isLoggedIn) {
