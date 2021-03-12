f940add457752b53eb4287cc7b3fc11a8db5df6b
toycrane <alwaysfun2183@gmail.com>
2021-03-12 16:58:56 +0900

    DropwDown 초기화

---
+++ src/components/Molecules/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dropdown from ".";
+export default {
+	title: "Organisms/Dropdown",
+	component: Dropdown,
+};
+
+export const LogoutStatusHeader = () => <Dropdown></Dropdown>;
