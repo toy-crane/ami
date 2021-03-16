d25825f59cddc8ef5a6ec7854cc018cbb43139af
toycrane <alwaysfun2183@gmail.com>
2021-03-16 16:15:59 +0900

    activate 여부에 따라 페이지 이동

--- src/Router/PrivateRoute/index.tsx
+++ src/Router/PrivateRoute/index.tsx
@@ -5,11 +5,20 @@ import { Route, Redirect, RouteProps } from "react-router-dom";
 
 // 로그인 필요 페이지 전용 Route
 const PrivateRoute = ({ component: Component, ...rest }: any) => {
-	const { isLoggedIn } = useReactiveVar(accountInfoCache);
+	const { isLoggedIn, isActive } = useReactiveVar(accountInfoCache);
 
 	const handleRoute = (routeProps: RouteProps) => {
-		if (isLoggedIn) {
+		if (isLoggedIn && isActive) {
 			return <Component {...routeProps}></Component>;
+		} else if (isLoggedIn && !isActive) {
+			return (
+				<Redirect
+					to={{
+						pathname: "/activate",
+						state: { referer: routeProps.location },
+					}}
+				/>
+			);
 		} else {
 			return (
 				<Redirect
