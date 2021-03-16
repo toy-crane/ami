ab69f1b069c8a384d2a9ea38f24227759258deaf
toycrane <alwaysfun2183@gmail.com>
2021-03-16 10:01:51 +0900

    storybook Apollo Client 셋팅

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,8 +3,14 @@ import { addDecorator } from "@storybook/react";
 import { jsx, ThemeProvider } from "theme-ui";
 import theme from "../src/Styles/Theme";
 import StoryRouter from "storybook-react-router";
+import { ApolloProvider } from "@apollo/client";
+import client from "../src/apollo/client";
 
 // theme provider 사용하기
 addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
 // react router 사용
 addDecorator(StoryRouter());
+// apollo cache 사용하기
+addDecorator((story) => (
+	<ApolloProvider client={client}>{story()}</ApolloProvider>
+));
