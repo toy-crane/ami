a8464c1b50c3abb24fae00569bfeb5ceb6399592
toycrane <alwaysfun2183@gmail.com>
2021-03-17 00:09:27 +0900

    Activate Route에서 안쓰는 코드 삭제

--- src/Router/ActivateRoute/index.tsx
+++ src/Router/ActivateRoute/index.tsx
@@ -1,11 +1,10 @@
+import React from "react";
 import { useReactiveVar } from "@apollo/client";
 import { accountInfoCache } from "caches";
-import React from "react";
 import { Route, Redirect, RouteProps } from "react-router-dom";
 
 const ActivateRoute = ({ component: Component, ...rest }: any) => {
 	const { isLoggedIn, isActive } = useReactiveVar(accountInfoCache);
-	console.log(isLoggedIn, isActive);
 
 	const handleRoute = (routeProps: RouteProps) => {
 		if (isLoggedIn && !isActive) {
