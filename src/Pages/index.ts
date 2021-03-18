c6a762b3fb62efa7a99c84cf8a5e62cc7e4662c4
toycrane <alwaysfun2183@gmail.com>
2021-03-18 18:43:31 +0900

    index에 Activate Page 추가

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,3 +2,4 @@ export { default as Main } from "./Main";
 export { default as MyPage } from "./MyPage";
 export { default as Login } from "./Login";
 export { default as SignUp } from "./SignUp";
+export { default as Activate } from "./Activate";
